a05eef132774a7ff381e5d030254d63db23b0a60
theayze <fahmimikail86@gmail.com>
2020-08-13 01:04:05 +0800

    fixed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,7 +10,7 @@
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
         <!-- Styles -->
-        <style>/*
+        <style>
             html, body {
                 background-color: #fff;
                 color: #636b6f;
@@ -60,7 +60,7 @@
 
             .m-b-md {
                 margin-bottom: 30px;
-            } */
+            } 
         </style>
     </head>
     <body>

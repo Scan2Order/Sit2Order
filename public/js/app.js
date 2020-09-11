916daf3919d5a99c1fab3e6cd71707506fe31a79
theayze <fahmimikail86@gmail.com>
2020-09-11 11:41:03 +0800

    11septsuperadminfixed

--- public/js/app.js
+++ public/js/app.js
@@ -1908,21 +1908,6 @@ module.exports = {
 
 "use strict";
 __webpack_require__.r(__webpack_exports__);
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
 /* harmony default export */ __webpack_exports__["default"] = ({
   mounted: function mounted() {
     console.log("Component mounted.");
@@ -38370,44 +38355,6 @@ exports.clearImmediate = (typeof self !== "undefined" && self.clearImmediate) ||
 
 /***/ }),
 
-/***/ "./node_modules/vue-loader/lib/loaders/templateLoader.js?!./node_modules/vue-loader/lib/index.js?!./resources/js/components/Admin/AdminDashRest.vue?vue&type=template&id=2d1d5b7a&":
-/*!**********************************************************************************************************************************************************************************************************************!*\
-  !*** ./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/vue-loader/lib??vue-loader-options!./resources/js/components/Admin/AdminDashRest.vue?vue&type=template&id=2d1d5b7a& ***!
-  \**********************************************************************************************************************************************************************************************************************/
-/*! exports provided: render, staticRenderFns */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-__webpack_require__.r(__webpack_exports__);
-/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "render", function() { return render; });
-/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "staticRenderFns", function() { return staticRenderFns; });
-var render = function() {
-  var _vm = this
-  var _h = _vm.$createElement
-  var _c = _vm._self._c || _h
-  return _c("div", [
-    _c("table", { staticClass: "table table-dark" }, [
-      _c("tbody", [
-        _c("tr", [
-          _c("td", [_vm._v(_vm._s(_vm.id))]),
-          _vm._v(" "),
-          _c("td", [_vm._v(_vm._s(_vm.name))]),
-          _vm._v(" "),
-          _c("td", [_vm._v(_vm._s(_vm.address))]),
-          _vm._v(" "),
-          _c("td", [_vm._v(_vm._s(_vm.phone))])
-        ])
-      ])
-    ])
-  ])
-}
-var staticRenderFns = []
-render._withStripped = true
-
-
-
-/***/ }),
-
 /***/ "./node_modules/vue-loader/lib/loaders/templateLoader.js?!./node_modules/vue-loader/lib/index.js?!./resources/js/components/Admin/totalRestaurant.vue?vue&type=template&id=137335f4&scoped=true&":
 /*!************************************************************************************************************************************************************************************************************************************!*\
   !*** ./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/vue-loader/lib??vue-loader-options!./resources/js/components/Admin/totalRestaurant.vue?vue&type=template&id=137335f4&scoped=true& ***!
@@ -50875,20 +50822,19 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 "use strict";
 __webpack_require__.r(__webpack_exports__);
-/* harmony import */ var _AdminDashRest_vue_vue_type_template_id_2d1d5b7a___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./AdminDashRest.vue?vue&type=template&id=2d1d5b7a& */ "./resources/js/components/Admin/AdminDashRest.vue?vue&type=template&id=2d1d5b7a&");
-/* harmony import */ var _AdminDashRest_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./AdminDashRest.vue?vue&type=script&lang=js& */ "./resources/js/components/Admin/AdminDashRest.vue?vue&type=script&lang=js&");
-/* empty/unused harmony star reexport *//* harmony import */ var _node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../../../../node_modules/vue-loader/lib/runtime/componentNormalizer.js */ "./node_modules/vue-loader/lib/runtime/componentNormalizer.js");
-
+/* harmony import */ var _AdminDashRest_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./AdminDashRest.vue?vue&type=script&lang=js& */ "./resources/js/components/Admin/AdminDashRest.vue?vue&type=script&lang=js&");
+/* empty/unused harmony star reexport *//* harmony import */ var _node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../../../../node_modules/vue-loader/lib/runtime/componentNormalizer.js */ "./node_modules/vue-loader/lib/runtime/componentNormalizer.js");
+var render, staticRenderFns
 
 
 
 
 /* normalize component */
 
-var component = Object(_node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__["default"])(
-  _AdminDashRest_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__["default"],
-  _AdminDashRest_vue_vue_type_template_id_2d1d5b7a___WEBPACK_IMPORTED_MODULE_0__["render"],
-  _AdminDashRest_vue_vue_type_template_id_2d1d5b7a___WEBPACK_IMPORTED_MODULE_0__["staticRenderFns"],
+var component = Object(_node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_1__["default"])(
+  _AdminDashRest_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0__["default"],
+  render,
+  staticRenderFns,
   false,
   null,
   null,
@@ -50914,24 +50860,6 @@ component.options.__file = "resources/js/components/Admin/AdminDashRest.vue"
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _node_modules_babel_loader_lib_index_js_ref_4_0_node_modules_vue_loader_lib_index_js_vue_loader_options_AdminDashRest_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! -!../../../../node_modules/babel-loader/lib??ref--4-0!../../../../node_modules/vue-loader/lib??vue-loader-options!./AdminDashRest.vue?vue&type=script&lang=js& */ "./node_modules/babel-loader/lib/index.js?!./node_modules/vue-loader/lib/index.js?!./resources/js/components/Admin/AdminDashRest.vue?vue&type=script&lang=js&");
 /* empty/unused harmony star reexport */ /* harmony default export */ __webpack_exports__["default"] = (_node_modules_babel_loader_lib_index_js_ref_4_0_node_modules_vue_loader_lib_index_js_vue_loader_options_AdminDashRest_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0__["default"]); 
-
-/***/ }),
-
-/***/ "./resources/js/components/Admin/AdminDashRest.vue?vue&type=template&id=2d1d5b7a&":
-/*!****************************************************************************************!*\
-  !*** ./resources/js/components/Admin/AdminDashRest.vue?vue&type=template&id=2d1d5b7a& ***!
-  \****************************************************************************************/
-/*! exports provided: render, staticRenderFns */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-__webpack_require__.r(__webpack_exports__);
-/* harmony import */ var _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_AdminDashRest_vue_vue_type_template_id_2d1d5b7a___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! -!../../../../node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!../../../../node_modules/vue-loader/lib??vue-loader-options!./AdminDashRest.vue?vue&type=template&id=2d1d5b7a& */ "./node_modules/vue-loader/lib/loaders/templateLoader.js?!./node_modules/vue-loader/lib/index.js?!./resources/js/components/Admin/AdminDashRest.vue?vue&type=template&id=2d1d5b7a&");
-/* harmony reexport (safe) */ __webpack_require__.d(__webpack_exports__, "render", function() { return _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_AdminDashRest_vue_vue_type_template_id_2d1d5b7a___WEBPACK_IMPORTED_MODULE_0__["render"]; });
-
-/* harmony reexport (safe) */ __webpack_require__.d(__webpack_exports__, "staticRenderFns", function() { return _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_AdminDashRest_vue_vue_type_template_id_2d1d5b7a___WEBPACK_IMPORTED_MODULE_0__["staticRenderFns"]; });
-
-
 
 /***/ }),
 
--- resources/views/Admin/admin-assets/AddUser.blade.php
+++ resources/views/Admin/admin-assets/AddUser.blade.php
@@ -2,43 +2,43 @@
 
 @section('content')
 <div class="othercontainer">
-  <div class="">
-    <div class="row justify-content-center">
-      <div class="col-md-8">
-        <div class="basecard">
-          <div class="card-header">Add User</div>
+    <div class="">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="basecard">
+                    <div class="card-header">Add User</div>
 
-          <div class="card-body">
+                        <div class="card-body">
 
-  <table class="table table-bordered table-striped">
-    <thead>
-      <tr>
-        <th style="width: 5%;">ID</th>
-        <th >Name</th>
-        <th >Email</th>
-        <th >Gender</th>
-        <th >Phone</th>
-      </tr>
-    </thead>
-    <tbody>
-      @foreach ($users as $user)
-      <tr>
-       <th style="width: 5%;">{{$user->id}}</th>
-       <td class="norm-space">{{$user->name}}</td>
-       <td class="norm-space">{{$user->email}}</td>
-       <td class="norm-space">{{$user->gender}}</td>
-       <td class="norm-space">{{$user->phone}}</td>
-     </tr>
-     @endforeach
+                            <table class="table table-bordered table-striped">
+                                <thead>
+                                    <tr>
+                                        <th style="width: 5%;">ID</th>
+                                        <th >Name</th>
+                                        <th >Email</th>
+                                        <th >Gender</th>
+                                        <th >Phone</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach ($users as $user)
+                                    <tr>
+                                        <th style="width: 5%;">{{$user->id}}</th>
+                                        <td class="norm-space">{{$user->name}}</td>
+                                        <td class="norm-space">{{$user->email}}</td>
+                                        <td class="norm-space">{{$user->gender}}</td>
+                                        <td class="norm-space">{{$user->phone}}</td>
+                                    </tr>
+                                    @endforeach
 
-    </tbody>
-  </table>
-          </div>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
     </div>
-  </div>
-</div>
 <br />
 @foreach ($users as $user)
 <user-show id="{{$user->id}}" name="{{$user->name}}" email="{{$user->email}}" gender="{{$user->gender}}" phone="{{$user->phone}}"></user-show>

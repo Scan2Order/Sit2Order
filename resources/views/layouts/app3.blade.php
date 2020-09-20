8fd11051023ff80d9cf9dce78d02a0cc44a09c53
jjs9000 <farriszamrie15@gmail.com>
2020-09-20 18:32:33 +0800

    hello

--- resources/views/layouts/app3.blade.php
+++ resources/views/layouts/app3.blade.php
@@ -26,7 +26,7 @@
     <nav class="navbar navbar-expand-md navbar-dark shadow-sm" style="background-color: #202020">
       <div class="container">
         <img src="./images/logo1.png" />
-        <a class="navbar-brand" href="{{ url('/') }}">
+        <a class="navbar-brand" href="{{ url('/restaurant') }}">
           {{ config('app.name', 'Laravel') }}
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
